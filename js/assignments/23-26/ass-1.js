0a6c31eac28de547c7ff79a8152e94cc0ca9bc50
Abdelrahman <bedoelsayed785@gmail.com>
2025-12-10 23:37:10 +0200

    feat: lessons 23-26 and assignments

---
+++ js/assignments/23-26/ass-1.js
@@ -0,0 +1,16 @@
+// Examples
+console.log(100_000); // 100000
+console.log(100000); // 100000
+console.log(5e4 + 5e4); // 100000
+
+// Your Solutions
+console.log(1e5); // 100000
+console.log(1e2 * 1e3); // 100000
+console.log(10 ** 5); // 100000
+console.log(6e4 + 4e4); // 100000
+console.log(5e2 * 2e2); // 100000
+console.log(40 * 2500); // 100000
+console.log(Math.pow(10, 5)); // 100000
+console.log(Math.floor(100_000.99999)); // 100000
+console.log(1e6 / 10); // 100000
+console.log(100_000_000 / 1000); // 100000
---
+++ js/assignments/23-26/ass-3.js
@@ -0,0 +1,2 @@
+console.log(Math.ceil(Math.log10(Number.MAX_SAFE_INTEGER)));
+console.log(Number.MAX_SAFE_INTEGER.toString().length);
