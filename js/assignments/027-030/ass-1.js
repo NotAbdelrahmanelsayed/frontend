c503e0a98a00ce8f6470aba9489f314649d11f03
Abdelrahman <bedoelsayed785@gmail.com>
2025-12-11 00:25:45 +0200

    feat: lessons 27-30 and assignments

---
+++ js/assignments/027-030/ass-1.js
@@ -0,0 +1,7 @@
+let userName = "Elzero";
+console.log(userName[0].toLowerCase()); // e
+console.log(userName.charAt(3)); // e
+console.log(userName.substring(0, 1).toLowerCase()); // e
+console.log(userName.slice(3, 4)); // e
+console.log(userName[3]); // e
+console.log(userName.charAt(3).repeat(3)); // eee
