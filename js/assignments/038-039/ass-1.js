cda182a0423af4f274884fc400dbabd7e12f2fb8
Abdelrahman <bedoelsayed785@gmail.com>
2025-12-13 11:14:25 +0200

    feat: solve assignment 38-39

---
+++ js/assignments/038-039/ass-1.js
@@ -0,0 +1,41 @@
+let day = "   friday  ";
+// // You Need To Remove Spaces And Make First Letter Capital => Friday
+
+// let day = "Friday";
+// let day = "Saturday";
+// let day = "Sunday";
+// // Output => "No Appointments Available"
+
+// let day = "Monday";
+// let day = "Thursday";
+// // Output => "From 10:00 AM To 5:00 PM"
+
+// let day = "Tuesday";
+// // Output => "From 10:00 AM To 6:00 PM"
+
+// let day = "Wednesday";
+// // Output => "From 10:00 AM To 7:00 PM"
+
+// let day = "World";
+// // Output => "Its Not A Valid Day"
+day = day.trim().toLowerCase();
+switch (day) {
+  case "friday":
+  case "saturday":
+  case "sunday":
+    console.log("No Appointments Available");
+    break;
+  case "monday":
+  case "thursday":
+    console.log("From 10:00 AM To 5:00 PM");
+    break;
+  case "tuesday":
+    console.log("From 10:00 AM To 6:00 PM");
+    break;
+  case "wednesday":
+    console.log("From 10:00 AM To 7:00 PM");
+    break;
+  default:
+    console.log("Its Not A Valid Day");
+    break;
+}
